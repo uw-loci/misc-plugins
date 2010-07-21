a590c5bcbc22e9824ffc15ea39dba4ded7b6addf
Aivar Grislis <grislis@wisc.edu>
2010-07-21 23:25:31 +0000

    Exports a text file with x, y, roi number, and intensity data.

---
+++ Export_As_Text/src/Export_As_Text.java
@@ -0,0 +1,147 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.gui.Roi;
+import ij.plugin.filter.PlugInFilter;
+import ij.plugin.frame.RoiManager;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.prefs.*;
+
+public class Export_As_Text implements PlugInFilter {
+    private static final String FILE_KEY = "export_as_text_file";
+    private String m_file;
+    private ImagePlus imp;
+    private Roi[] m_rois = {};
+
+    public int setup(String arg, ImagePlus imp) {
+        this.imp = imp;
+        return DOES_ALL;
+    }
+
+    public void run(ImageProcessor ip) {
+        boolean floatType = ip instanceof FloatProcessor;
+
+        // get list of current ROIs
+        RoiManager manager = RoiManager.getInstance();
+        if (null != manager) {
+            m_rois = manager.getRoisAsArray();
+            //IJ.log("Got ROI Manager, count " + m_rois.length);
+        }
+        else IJ.log("No ROI Manager");
+
+        //byte[] byteArray = (byte[]) ip.getPixels();
+
+        if (showFileDialog(getFileFromPreferences())) {
+            saveFileInPreferences(m_file);
+        }
+        else {
+            return;
+        }
+
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(m_file);
+        } catch (IOException e) {
+            IJ.log("exception opening file " + m_file);
+            IJ.handleException(e);
+        }
+
+        //StringBuffer sb = new StringBuffer();
+//
+        //for (byte b : byteArray)
+        //{
+        //    sb.append( b );
+        //    sb.append("\t");
+        //}
+
+        try
+        {
+            //Write the header
+            fw.write("x\ty\tROI\tIntensity\n");
+
+            for(int x = 0; x < ip.getWidth(); x++)
+            {
+                for(int y = 0; y < ip.getHeight(); y++)
+                {
+                    fw.write( x  + "\t" + y + "\t" + lookUpRoi(x, y) + "\t" + getPixel(floatType, ip, x, y)  + "\n" );
+                    //if (0 != lookUpRoi(x,y)) IJ.log(" x " + x + " y " + y + " roi " + lookUpRoi(x,y) + " intensity " + getPixel(floatType, ip, x, y));
+                }
+            }
+            fw.close();
+        }
+        catch (IOException e)
+        {
+            IJ.log("exception writing file");
+            IJ.handleException(e);
+        }
+
+        //IJ.log( sb.toString() );
+        imp.updateAndDraw();
+    }
+
+    private String getFileFromPreferences() {
+       Preferences prefs = Preferences.userNodeForPackage(this.getClass());
+       return prefs.get(FILE_KEY, "");
+    }
+
+    private void saveFileInPreferences(String file) {
+        Preferences prefs = Preferences.userNodeForPackage(this.getClass());
+        prefs.put(FILE_KEY, file);
+    }
+
+    private boolean showFileDialog(String defaultFile) {
+        //TODO shouldn't UI be in separate class?
+        GenericDialog dialog = new GenericDialog("Export Image As Text");
+        dialog.addStringField("Save As:", defaultFile, 24);
+        dialog.showDialog();
+        if (dialog.wasCanceled()) {
+            return false;
+        }
+
+        m_file = dialog.getNextString();
+        return true;
+    }
+
+    /**
+     * Returns Roi number of a given pixel.
+     *
+     * @param x pixel x
+     * @param y pixel y
+     * @return which Roi number, 0 for none
+     */
+    private int lookUpRoi(int x, int y) {
+        for (int i = 0; i < m_rois.length; ++i) {
+            if (m_rois[i].contains(x, y)) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Returns pixel value.
+     *
+     * @param floatType whether ImageProcessor is a FloatProcessor
+     * @param ip ImageProcessor
+     * @param x pixel x
+     * @param y pixel y
+     * @return pixel value
+     */
+    private Object getPixel(boolean floatType, ImageProcessor ip, int x, int y) {
+        Object returnValue = null;
+        int pixel = ip.getPixel(x,y);
+        if (floatType) {
+            returnValue = Float.intBitsToFloat(pixel);
+        }
+        else {
+            returnValue = pixel;
+        }
+        return returnValue;
+    }
+
+}
