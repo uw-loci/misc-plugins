bf2defbd0994ddc03c9fb1adff09e5578c8c43f4
Curtis Rueden <ctrueden@wisc.edu>
2011-09-13 20:33:40 +0000

    Progress on Prairie TIFF stitching. Seems to work.
    Single-image TIFFs are compiled into TIFF stacks for compatibility with
    "Stitch Collection of Images" plugin. Stage coordinates are also
    normalized so that tiles start at (0, 0) so that the stitching plugin
    does not allocate needless black space when creating the mosaic.

--- src/main/java/loci/plugins/StitchPrairieTiff.java
+++ src/main/java/loci/plugins/StitchPrairieTiff.java
@@ -45,6 +45,7 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -53,6 +54,16 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+
+import loci.formats.CoreMetadata;
+import loci.formats.FormatException;
+import loci.formats.IFormatReader;
+import loci.formats.IFormatWriter;
+import loci.formats.MetadataTools;
+import loci.formats.in.MinimalTiffReader;
+import loci.formats.meta.IMetadata;
+import loci.formats.out.TiffWriter;
+import ome.xml.model.primitives.PositiveInteger;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -89,13 +100,13 @@ public class StitchPrairieTiff implements PlugIn {
 		final boolean success = chooseOptions();
 		if (!success) return;
 
-		final String tileConfigPath = createTileConfiguration(file);
+		final File tileFile = createTileConfiguration(file);
 		IJ.showStatus("");
-		if (tileConfigPath == null) return;
+		if (tileFile == null) return;
 
 		if (stitch) {
 			IJ.showStatus("Performing stitching...");
-			executeStitching(tileConfigPath);
+			executeStitching(tileFile.getAbsolutePath());
 			IJ.showStatus("Stitching complete.");
 		}
 	}
@@ -152,23 +163,154 @@ public class StitchPrairieTiff implements PlugIn {
 	 * Generates the tile configuration file for use with the Fiji Stitching
 	 * plugins.
 	 */
-	private String createTileConfiguration(final File dataFile) {
+	private File createTileConfiguration(final File dataFile) {
+		final File baseDir = dataFile.getParentFile();
 		try {
 			IJ.showStatus("Parsing stage positions...");
 			final List<Pt> coords = parseCoords(dataFile);
+			Collections.sort(coords);
+
+			IJ.showStatus("Normalizing coordinates...");
+			normalizeCoords(coords);
+
+			IJ.showStatus("Converting TIFFs to stacks...");
+			final List<Pt> newCoords = convertTIFFs(baseDir, coords);
 
 			IJ.showStatus("Generating tile configuration file...");
-			final File tileFile = writeTileFile(dataFile.getParentFile(), coords);
+			final File tileFile = writeTileFile(dataFile.getParentFile(), newCoords);
 			IJ.log("Wrote tile configuration:");
 			IJ.log(tileFile.getAbsolutePath());
 			IJ.log("");
 
-			return tileFile.getAbsolutePath();
+			return tileFile;
+		}
+		catch (final FormatException e) {
+			IJ.handleException(e);
 		}
 		catch (final IOException e) {
 			IJ.handleException(e);
 		}
 		return null;
+	}
+
+	/** Ensures coordinates begin at (0, 0). */
+	private void normalizeCoords(List<Pt> coords) {
+		// find minimum coordinates
+		double minX = Double.POSITIVE_INFINITY, minY = Double.POSITIVE_INFINITY;
+		for (Pt pt : coords) {
+			if (pt.x < minX) minX = pt.x;
+			if (pt.y < minY) minY = pt.y;
+		}
+		// normalize coordinates so minimum is (0, 0)
+		for (Pt pt : coords) {
+			pt.x -= minX;
+			pt.y -= minY;
+		}
+	}
+
+	/**
+	 * Compiles the collection of single-plane TIFFs into TIFF stacks along Z, one
+	 * stack per tile.
+	 */
+	private List<Pt> convertTIFFs(final File baseDir, final List<Pt> coords)
+		throws FormatException, IOException
+	{
+		if (coords == null || coords.size() == 0) return null;
+		final int firstIndex = coords.get(0).gridIndex;
+
+		final ArrayList<Pt> newCoords = new ArrayList<Pt>();
+
+		// compute list of stack sizes
+		final ArrayList<Integer> stackSizes = computeStackSizes(coords, firstIndex);
+
+		// convert individual TIFF files to TIFF stacks
+		final IFormatReader reader = new MinimalTiffReader();
+		final IFormatWriter writer = new TiffWriter();
+		int outIndex = 0;
+		boolean outValid = false;
+		String outName = null;
+		byte[] buf = null;
+		int lastIndex = 0;
+		int p = -1;
+		for (final Pt pt : coords) {
+			p++;
+			IJ.showStatus("Processing plane " + p + "/" + coords.size());
+			IJ.showProgress(p, coords.size());
+
+			if (pt.gridIndex != lastIndex) {
+				// new tile has begun
+				lastIndex = pt.gridIndex;
+
+				outName = "ZStack-" + pt.filename.replaceAll("_[0-9]+\\.tif", ".tif");
+				final File outFile = new File(baseDir, outName);
+				outValid = !outFile.exists();
+
+				final Pt newPt = new Pt();
+				newCoords.add(newPt);
+				newPt.filename = outName;
+				newPt.x = pt.x;
+				newPt.y = pt.y;
+				newPt.z = pt.z;
+
+				writer.close();
+				outIndex = 0;
+			}
+			if (!outValid) continue; // output stack already written
+
+			// read planes from input and write to output
+			final String inId = new File(baseDir, pt.filename).getAbsolutePath();
+			final IMetadata meta = MetadataTools.createOMEXMLMetadata();
+			reader.setMetadataStore(meta);
+			reader.setId(inId);
+			final int imageCount = reader.getImageCount();
+			for (int i = 0; i < imageCount; i++) {
+				if (buf == null) {
+					// allocate new buffer
+					buf = reader.openBytes(i);
+				}
+				else {
+					// reuse allocated buffer
+					reader.openBytes(i, buf);
+				}
+				if (outIndex == 0) {
+					// initialize fresh writer's dimensional metadata
+					final IMetadata outMeta = MetadataTools.createOMEXMLMetadata();
+					final CoreMetadata core = reader.getCoreMetadata()[0];
+					MetadataTools.populateMetadata(outMeta, 0, null, core);
+					final int sizeZ = stackSizes.get(0);
+					stackSizes.remove(0); // lazy
+					outMeta.setPixelsSizeZ(new PositiveInteger(sizeZ), 0);
+					writer.setMetadataRetrieve(outMeta);
+					final String outId = new File(baseDir, outName).getAbsolutePath();
+					writer.setId(outId);
+				}
+				writer.saveBytes(outIndex++, buf);
+			}
+			reader.close();
+		}
+		writer.close();
+
+		return newCoords;
+	}
+
+	private ArrayList<Integer> computeStackSizes(final List<Pt> coords,
+		final int firstIndex)
+	{
+		final ArrayList<Integer> stackSizes = new ArrayList<Integer>();
+		int stackSize = 0;
+		int lastIndex = firstIndex;
+		for (final Pt pt : coords) {
+			if (pt.gridIndex == lastIndex) {
+				stackSize++;
+			}
+			else {
+				stackSizes.add(stackSize);
+				stackSize = 1;
+				lastIndex = pt.gridIndex;
+			}
+		}
+		stackSizes.add(stackSize);
+		return stackSizes;
 	}
 
 	/** Executes the stitching plugin using the given tile configuration. */
@@ -223,21 +365,24 @@ public class StitchPrairieTiff implements PlugIn {
 	 * Writes the tile configuration to disk.
 	 * 
 	 * @param dir The directory where the data files are.
-	 * @return The full path to the file where tile configuration was written.
+	 * @return The full path to the file where tile configurations was written.
 	 * @throws IOException If something goes wrong writing the file.
 	 */
 	private static File writeTileFile(final File dir, final List<Pt> coords)
 		throws IOException
 	{
-		// create tile configuration file
+		// get current datestamp
 		final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd'-'HHmmss");
 		final Date now = Calendar.getInstance().getTime();
+		final String datestamp = sdf.format(now);
+
+		// create tile configuration file
 		final File tileFile =
-			new File(dir, "TileConfiguration-" + sdf.format(now) + ".txt");
+			new File(dir, "TileConfiguration-" + datestamp + ".txt");
 		final PrintWriter out = new PrintWriter(tileFile);
 
 		// write data to file
-		writeHeader(out, 3);
+		writeHeader(out);
 		for (final Pt pt : coords) {
 			writeLine(out, dir, pt.filename, 3, pt.x, pt.y, pt.z);
 		}
@@ -246,9 +391,9 @@ public class StitchPrairieTiff implements PlugIn {
 		return tileFile;
 	}
 
-	private static void writeHeader(final PrintWriter out, final int dim) {
+	private static void writeHeader(final PrintWriter out) {
 		out.println("# Define the number of dimensions we are working on");
-		out.println("dim = " + dim);
+		out.println("dim = 3");
 		out.println();
 		out.println("# Define the image coordinates");
 	}
@@ -311,12 +456,19 @@ public class StitchPrairieTiff implements PlugIn {
 		}
 	}
 
-	// -- Helper classes --
-
-	static class Pt {
+	static class Pt implements Comparable<Pt> {
 
 		String filename;
 		double x, y, z;
+		int gridIndex;
+
+		@Override
+		public int compareTo(final Pt pt) {
+			if (gridIndex < pt.gridIndex) return gridIndex - pt.gridIndex;
+			if (z != pt.z) return z < pt.z ? -1 : 1;
+			return filename.compareTo(pt.filename);
+		}
+
 	}
 
 	static class PrairieHandler extends DefaultHandler {
@@ -326,6 +478,7 @@ public class StitchPrairieTiff implements PlugIn {
 		private String filename;
 		private double x, y, z;
 		private double xMicrons, yMicrons;
+		private int gridIndex;
 
 		PrairieHandler(final List<Pt> coords) {
 			this.coords = coords;
@@ -357,6 +510,9 @@ public class StitchPrairieTiff implements PlugIn {
 				else if (key.equals("micronsPerPixel_YAxis")) {
 					yMicrons = Double.parseDouble(value);
 				}
+				else if (key.equals("xYStageGridIndex")) {
+					gridIndex = Integer.parseInt(value);
+				}
 			}
 		}
 
@@ -370,6 +526,7 @@ public class StitchPrairieTiff implements PlugIn {
 				pt.x = x / xMicrons;
 				pt.y = y / yMicrons;
 				pt.z = z;
+				pt.gridIndex = gridIndex;
 				coords.add(pt);
 				clearState();
 			}
@@ -379,6 +536,7 @@ public class StitchPrairieTiff implements PlugIn {
 			filename = null;
 			x = y = z = Double.NaN;
 			xMicrons = yMicrons = 1;
+			gridIndex = 0;
 		}
 
 	}
