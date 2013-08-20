92fac127385c71086040f83c8b66d52d36a00d18
Curtis Rueden <ctrueden@wisc.edu>
2013-08-19 22:33:43 -0500

    VisualizeTiles: first cut at showing tiles as ROIs
    Ostensibly works, though still needs some refinement. In particular,
    there may be a problem with stage coordinate units (pixels vs. microns)
    being misinterpreted.

--- src/main/java/loci/plugins/VisualizeTiles.java
+++ src/main/java/loci/plugins/VisualizeTiles.java
@@ -22,26 +22,27 @@
 package loci.plugins;
 
 import ij.IJ;
+import ij.ImageJ;
 import ij.ImagePlus;
+import ij.gui.Roi;
 import ij.io.OpenDialog;
 import ij.plugin.PlugIn;
+import ij.plugin.frame.RoiManager;
 import ij.process.ByteProcessor;
 
 import java.awt.Dimension;
-import java.awt.Polygon;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import loci.formats.FormatException;
-import loci.formats.tiff.IFD;
-import loci.formats.tiff.TiffParser;
+import loci.formats.ImageReader;
+import loci.formats.MetadataTools;
+import loci.formats.meta.IMetadata;
 
 /**
- * A plugin to visualize the tile layout of a tile configuration file.
+ * A plugin to visualize the tile layout of a multi-tile dataset.
  * 
  * @author Curtis Rueden
  */
@@ -63,12 +64,18 @@ public class VisualizeTiles implements PlugIn {
 		}
 	}
 
+	// -- Main method --
+
+	public static void main(final String... args) {
+		new ImageJ();
+		IJ.runPlugIn(VisualizeTiles.class.getName(), "");
+	}
+
 	// -- Helper methods --
 
 	/** Prompts the user to choose a file. */
 	private File chooseFile() {
-		final OpenDialog od =
-			new OpenDialog("Choose a tile configuration file", "");
+		final OpenDialog od = new OpenDialog("Choose a data file", "");
 		final String name = od.getFileName();
 		final String dir = od.getDirectory();
 		if (name == null || dir == null) {
@@ -89,65 +96,62 @@ public class VisualizeTiles implements PlugIn {
 		IJ.showStatus("Reading tile coordinates");
 		final List<Pt> coords = readCoords(file);
 
-		IJ.showStatus("Reading tile dimensions");
-		final String filename = coords.get(0).filename;
-		final String path =
-			new File(file.getParentFile(), filename).getAbsolutePath();
-		final Dimension tileDim = readImageDimensions(path);
-
 		IJ.showStatus("Calculating");
 		final Pt min = getMinPoint(coords), max = getMaxPoint(coords);
-		final Dimension mosaicDim = getMosaicDimensions(min, max, tileDim);
+		final Dimension mosaicDim = getMosaicDimensions(min, max);
 		if (mosaicDim == null) {
 			IJ.showStatus("");
 			return null;
 		}
 
-		IJ.showStatus("Painting image");
-		final ByteProcessor ip = drawTiles(coords, min, tileDim, mosaicDim);
-		final ImagePlus imp = new ImagePlus(file.getName(), ip);
-		IJ.run(imp, "glasbey", "");
+		IJ.showStatus("Creating image");
+		final ImagePlus imp = createImage(file.getName(), mosaicDim);
+
+		IJ.showStatus("Populating tile ROIs");
+		buildTileROIs(coords, min);
+
 		IJ.showStatus("");
 
 		return imp;
 	}
 
-	private List<Pt> readCoords(final File file) throws IOException {
-		if (!file.getName().startsWith("TileConfiguration")) {
-			IJ.log("WARNING: File '" + file.getPath() +
-				"' may not be a tile configuration file.");
-		}
-		final BufferedReader in = new BufferedReader(new FileReader(file));
-		final ArrayList<Pt> coords = new ArrayList<Pt>();
-		while (true) {
-			String line = in.readLine();
-			if (line == null) break; // eof
-			line = line.trim();
-			if (line.equals("") || line.startsWith("#")) continue;
-			if (line.startsWith("dim = ")) continue;
+	private List<Pt> readCoords(final File file) throws FormatException,
+		IOException
+	{
+		final ImageReader in = new ImageReader();
+		final IMetadata omeMeta = MetadataTools.createOMEXMLMetadata();
+		in.setMetadataStore(omeMeta);
+		in.setId(file.getAbsolutePath());
 
-			final int lParen = line.lastIndexOf("(");
-			final int rParen = line.lastIndexOf(")");
-			final String[] tokens = line.substring(lParen + 1, rParen).split(", ");
-			final Pt pt = new Pt();
-			pt.filename = line.substring(0, line.indexOf(";"));
-			pt.x = Double.parseDouble(tokens[0]);
-			pt.y = Double.parseDouble(tokens[1]);
-			coords.add(pt);
+		final ArrayList<Pt> coords = new ArrayList<Pt>();
+		final int imageCount = omeMeta.getImageCount();
+		for (int i = 0; i < imageCount; i++) {
+			in.setSeries(i);
+			final int sizeX = in.getSizeX();
+			final int sizeY = in.getSizeY();
+			final int planeCount = omeMeta.getPlaneCount(i);
+			for (int p = 0; p < planeCount; p++) {
+				final Integer c = omeMeta.getPlaneTheC(i, p).getValue();
+				final Integer z = omeMeta.getPlaneTheZ(i, p).getValue();
+				final Integer t = omeMeta.getPlaneTheT(i, p).getValue();
+				final Double posX = omeMeta.getPlanePositionX(i, p);
+				final Double posY = omeMeta.getPlanePositionY(i, p);
+				final Double posZ = omeMeta.getPlanePositionZ(i, p);
+				coords.add(new Pt(i, p, c, z, t, sizeX, sizeY, posX, posY, posZ, t));
+			}
+			try {
+				final Double labelX = omeMeta.getStageLabelX(i);
+				final Double labelY = omeMeta.getStageLabelY(i);
+				final Double labelZ = omeMeta.getStageLabelZ(i);
+				coords.add(new Pt(i, sizeX, sizeY, labelX, labelY, labelZ));
+			}
+			catch (final NullPointerException exc) {
+				// HACK: Workaround for bug in loci:ome-xml:4.4.8.
+			}
 		}
 		in.close();
-		return coords;
-	}
 
-	private Dimension readImageDimensions(final String id) throws IOException,
-		FormatException
-	{
-		final TiffParser tiffParser = new TiffParser(id);
-		final IFD ifd = tiffParser.getFirstIFD();
-		tiffParser.getStream().close();
-		final int w = (int) ifd.getImageWidth();
-		final int h = (int) ifd.getImageLength();
-		return new Dimension(w, h);
+		return coords;
 	}
 
 	private Pt getMinPoint(final List<Pt> coords) {
@@ -166,17 +170,17 @@ public class VisualizeTiles implements PlugIn {
 		max.x = Double.NEGATIVE_INFINITY;
 		max.y = Double.NEGATIVE_INFINITY;
 		for (final Pt pt : coords) {
-			if (pt.x > max.x) max.x = pt.x;
-			if (pt.y > max.y) max.y = pt.y;
+			final double x = pt.x + pt.w;
+			final double y = pt.y + pt.h;
+			if (x > max.x) max.x = x;
+			if (y > max.y) max.y = y;
 		}
 		return max;
 	}
 
-	private Dimension getMosaicDimensions(final Pt min, final Pt max,
-		final Dimension tileDim)
-	{
-		final int width = (int) (max.x - min.x) + tileDim.width;
-		final int height = (int) (max.y - min.y) + tileDim.height;
+	private Dimension getMosaicDimensions(final Pt min, final Pt max) {
+		final int width = (int) Math.ceil(max.x) - (int) Math.floor(min.x);
+		final int height = (int) Math.ceil(max.y) - (int) Math.floor(min.y);
 		if (width <= 0 || height <= 0) {
 			IJ.error("Invalid image dimensions: " + width + " x " + height);
 			return null;
@@ -188,50 +192,68 @@ public class VisualizeTiles implements PlugIn {
 		return new Dimension(width, height);
 	}
 
-	private ByteProcessor drawTiles(final List<Pt> coords, final Pt min,
-		final Dimension tileDim, final Dimension mosaicDim)
-	{
+	private ImagePlus createImage(final String title, final Dimension mosaicDim) {
 		final ByteProcessor ip =
 			new ByteProcessor(mosaicDim.width, mosaicDim.height);
+		final ImagePlus imp = new ImagePlus(title, ip);
+		imp.show();
+		return imp;
+	}
 
-		// draw tiles
-		final int[] xPoints = new int[4], yPoints = new int[4];
-		int color = 1;
+	private void buildTileROIs(final List<Pt> coords, final Pt min) {
+		IJ.run("ROI Manager...");
+		final RoiManager rm = RoiManager.getInstance();
 		for (final Pt pt : coords) {
-			final int x = (int) (pt.x - min.x), y = (int) (pt.y - min.y);
-			ip.setColor(color++);
-			if (color > 254) color = 1;
-			xPoints[0] = x;
-			yPoints[0] = y;
-			xPoints[1] = x + tileDim.width;
-			yPoints[1] = y;
-			xPoints[2] = x + tileDim.width;
-			yPoints[2] = y + tileDim.height;
-			xPoints[3] = x;
-			yPoints[3] = y + tileDim.height;
-			final Polygon rect = new Polygon(xPoints, yPoints, xPoints.length);
-			ip.fillPolygon(rect);
+			final Roi roi = new Roi(pt.x - min.x, pt.y - min.y, pt.w, pt.h);
+			roi.setName(pt.name());
+			rm.addRoi(roi);
 		}
-
-		// draw tile outlines
-		ip.setLineWidth(10);
-		color = 1;
-		for (final Pt pt : coords) {
-			final int x = (int) (pt.x - min.x), y = (int) (pt.y - min.y);
-			ip.setColor(color++);
-			if (color > 254) color = 1;
-			ip.drawRect(x, y, tileDim.width, tileDim.height);
-		}
-
-		return ip;
+		rm.runCommand("Show All");
 	}
 
 	// -- Helper classes --
 
 	static class Pt {
 
-		String filename;
-		double x, y;
+		int i, p;
+		int w, h;
+		int theC, theZ, theT;
+		double x, y, z;
+		int t;
+
+		public Pt() {}
+
+		public Pt(final int i, final int w, final int h, final Double x,
+			final Double y, final Double z)
+		{
+			this(i, -1, w, h, null, null, null, x, y, z, null);
+		}
+
+		public Pt(final int i, final int p, final int w, final int h,
+			final Integer theC, final Integer theZ, final Integer theT,
+			final Double x, final Double y, final Double z, final Integer t)
+		{
+			this.i = i;
+			this.p = p;
+			this.theC = theC == null ? -1 : theC;
+			this.theZ = theZ == null ? -1 : theZ;
+			this.theT = theT == null ? -1 : theT;
+			this.w = w;
+			this.h = h;
+			this.x = x == null ? Double.NaN : x;
+			this.y = y == null ? Double.NaN : y;
+			this.z = z == null ? Double.NaN : z;
+			this.t = t == null ? -1 : t;
+		}
+
+		public String name() {
+			final StringBuilder sb = new StringBuilder("image=" + i);
+			if (p >= 0) sb.append("; tile=" + p);
+			if (theC >= 0) sb.append("; C=" + theC);
+			if (theZ >= 0) sb.append("; Z=" + theZ);
+			if (theT >= 0) sb.append("; T=" + theT);
+			return sb.toString();
+		}
 	}
 
 }
